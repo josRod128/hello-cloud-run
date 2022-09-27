f6a62f5d928e465d4efaae95d88cf1df0df3ef2e
Jose Rodriguez <jose12836@gmail.com>
2022-09-26 23:42:23 -0600

    push prueba

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/', (req, res) => {
   res.json({
     date: today,
     up: `${(Date.now() - dateUp)/1000} seg.`,
-    repo: 'https://github.com/josRod128/hello-cloud-run'
+    repo: 'https://github.com/josRod128/hello-cloud-run',
+    text: 'Funciono el push'
   });
 });
 
